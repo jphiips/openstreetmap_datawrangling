06e5faa3493d451a69de1a5eb8387dc3f2d0867d
jphiips <83444347+jphiips@users.noreply.github.com>
2021-04-30 23:20:44 -0500

    Update zip_code_audit.py

--- zip_code_audit.py
+++ zip_code_audit.py
@@ -1,20 +1,14 @@
-# Auditing Postal Codes
-
-'''
-this audits postal codes in a very similar process to street auditing
-
-'''
 import xml.etree.cElementTree as ET
 from collections import defaultdict
 import re
 import pprint
 
-OSM_FILE = "germantown.osm"
-zip_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+dataset = "sample.osm"
 
+zip_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 zip_types = defaultdict(set)
 
-expected_zip = {38138, 38139, 38183}
+expected_zip = {'38138', '38139', '38183'}
 
 def audit_zip_codes(zip_types, zip_name, regex, expected_zip):
     m = regex.search(zip_name)
@@ -26,7 +20,7 @@ def audit_zip_codes(zip_types, zip_name, regex, expected_zip):
 def is_zip_name(elem):
     return (elem.attrib['k'] == "addr:postcode")
 
-
+#function to perform audit on the zip code data
 def audit(filename, regex):
     for event, elem in ET.iterparse(filename, events=("start",)):
         if elem.tag == "way" or elem.tag == "node":
@@ -35,21 +29,8 @@ def audit(filename, regex):
                     audit_zip_codes(zip_types, tag.attrib['v'], regex, expected_zip)
     pprint.pprint(dict(zip_types))
 
+ 
+audit(dataset, zip_type_re)
 
-
-    
-audit(OSM_FILE, zip_type_re)
-
-
-for zip_type, ways in zip_types.items(): 
-        for name in ways:
-            if "-" in name:
-                name = name.split("-")[0].strip()
-            if "TN " in name:
-                name = name.split("TN ")[1].strip('TN ')
-            elif len(str(name))>5:
-                name=name[0:5]
-            elif name.isdigit()==False:
-                print ('OK')
-            print (name)  
-
+#audit of zip codes shows a few zip codes which are not part of the Germantown area. 
+#the decidion was made to not clean this part of the dataset - the post codes represent neighboring areas
