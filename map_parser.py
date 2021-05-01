1e6c2c2eca26a432cfa1f441f14cdff73afb41ed
jphiips <83444347+jphiips@users.noreply.github.com>
2021-05-01 00:14:45 -0500

    Update map_parser.py

--- map_parser.py
+++ map_parser.py
@@ -9,7 +9,116 @@ import sys
 if sys.version_info[0] >= 3:
     unicode = str
 
-OSM_FILE = "germantown.osm"
+dataset = "sample.osm"
+
+schema = {
+    'node': {
+        'type': 'dict',
+        'schema': {
+            'id': {'required': True, 'type': 'integer', 'coerce': int},
+            'lat': {'required': True, 'type': 'float', 'coerce': float},
+            'lon': {'required': True, 'type': 'float', 'coerce': float},
+            'user': {'required': True, 'type': 'string'},
+            'uid': {'required': True, 'type': 'integer', 'coerce': int},
+            'version': {'required': True, 'type': 'string'},
+            'changeset': {'required': True, 'type': 'integer', 'coerce': int},
+            'timestamp': {'required': True, 'type': 'string'}
+        }
+    },
+    'node_tags': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'id': {'required': True, 'type': 'integer', 'coerce': int},
+                'key': {'required': True, 'type': 'string'},
+                'value': {'required': True, 'type': 'string'},
+                'type': {'required': True, 'type': 'string'}
+            }
+        }
+    },
+    'way': {
+        'type': 'dict',
+        'schema': {
+            'id': {'required': True, 'type': 'integer', 'coerce': int},
+            'user': {'required': True, 'type': 'string'},
+            'uid': {'required': True, 'type': 'integer', 'coerce': int},
+            'version': {'required': True, 'type': 'string'},
+            'changeset': {'required': True, 'type': 'integer', 'coerce': int},
+            'timestamp': {'required': True, 'type': 'string'}
+        }
+    },
+    'way_nodes': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'id': {'required': True, 'type': 'integer', 'coerce': int},
+                'node_id': {'required': True, 'type': 'integer', 'coerce': int},
+                'position': {'required': True, 'type': 'integer', 'coerce': int}
+            }
+        }
+    },
+    'way_tags': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'id': {'required': True, 'type': 'integer', 'coerce': int},
+                'key': {'required': True, 'type': 'string'},
+                'value': {'required': True, 'type': 'string'},
+                'type': {'required': True, 'type': 'string'}
+            }
+        }
+    }
+}
+
+# Getting the elements with tags - nodes and way
+
+def get_element(filename, tags=('node', 'way', 'relation')):
+    context = iter(ET.iterparse(filename, events=('start', 'end')))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+            
+# Cleaning up and Fixing the Street Names
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+
+expected = ['Avenue', 'Boulevard', 'Broadway', 'Commons', 'Court', 'Cove','Drive', 'Lane', 'Park', 'Parkway',
+            'Place', 'Road', 'Square', 'Street', 'Terrace', 'Trail', 'Turnpike', 'Creek','Way','Crossing','Lake',
+            'North', 'South', 'East', 'West','Pike','Point']
+
+mapping = {'Ave': 'Avenue',
+                'Ave.': 'Avenue',
+                'Ct': 'Court',
+                'Cv': 'Cove',
+                'Dr': 'Drive',
+                'Pkwy': 'Parkway',
+                'Rd': 'Road',
+                'St': 'Street',
+                'St.': 'Street',
+                'Elkgate':'Road',
+                'London' : 'Drive',
+                'Chauncey' : 'Way'
+                }
+
+
+def update_street_name(name, mapping):
+    for key,value in mapping.items():
+        if key in name:
+            return name.replace(key,value)
+    return name        
+
+def audit_street_name_tag(element): 
+    street_name=element.get('v')
+    m = street_type_re.search(street_name)
+    if m:
+        better_street_name=update_street_name(street_name,mapping)
+        return better_street_name
+    return street_name
 
 NODES_PATH = "nodes.csv"
 NODE_TAGS_PATH = "nodes_tags.csv"
@@ -30,98 +139,14 @@ WAY_FIELDS = ['id', 'user', 'uid', 'version', 'changeset', 'timestamp']
 WAY_TAGS_FIELDS = ['id', 'key', 'value', 'type']
 WAY_NODES_FIELDS = ['id', 'node_id', 'position']
 
-
-street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
-
-
-expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 
-            "Trail", "Parkway", "Commons","Freeway","Circle","Way","Highway", "Cove",
-            "Terrace","South","East","West","North"]
-
-# UPDATE THIS VARIABLE
-mapping = {
-            " St ": " Street ",
-            " St.": " Street ",
-            " Rd.": " Road ",
-            " Rd ": " Road ",
-            " Rd": " Road ",
-            " Ave ": " Avenue ", 
-            " Ave.": " Avenue ",
-            " Av ": " Avenue ", 
-            " Dr ": " Drive ",
-            " Dr.": " Drive",
-            " Blvd ": " Boulevard ",
-            " Blvd": " Boulevard",
-            " Blvd.": " Boulevard",
-            " Ct ": " Centre ",
-            " Ctr": " Centre",
-            " Pl ": " Place ",
-            " Ln ": " Lane ",
-            " Cir ": " Circle ",
-            "Cv" : "Cove",
-            " Wy": " Way ",
-            " S ": " South ",
-            " E ": " East ",
-            " W ": " West ",
-            " N ": "North"
-}
-
-
-'''
-The update name function implements the change. If a street name has the defined string which is defined in the mapping
-dictionary, then the change is made as defined.
-'''
-
-'''
-Below 2 functions would be used during shape_element function execution to formatt the street name
-
-'''
-
-def update_street_name(name, mapping):
-    for key,value in mapping.items():
-        if key in name:
-            return name.replace(key,value)
-    return name        
-
-def audit_street_name_tag(element): 
-    street_name=element.get('v')
-    m = street_type_re.search(street_name)
-    if m:
-        better_street_name=update_street_name(street_name,mapping)
-        return better_street_name
-    return street_name
- 
- def update_postcode(name): 
-    if "-" in name:
-        name = name.split("-")[0].strip()
-    elif "TN" in name:
-        name = name.split("TN ")[1].strip('TN ')
-    elif len(str(name))>5:
-        name=name[0:5]
-    elif name.isdigit()==False:
-         name=00000
-    return name
-
-
-
-def audit_postcode_tag(element,regex=re.compile(r'\b\S+\.?$', re.IGNORECASE)):
-    post_code=element.get('v')
-    m = regex.search(post_code)
-    if m:
-        better_postcode=update_postcode(post_code)
-        return better_postcode
-    return post_code   
-
-
 def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIELDS,
                   problem_chars=PROBLEMCHARS, default_tag_type='regular'):
-    """Clean and shape node or way XML element to Python dict"""    
+    """Clean and shape node or way XML element to Python dict"""
     
     node_attribs = {}
     way_attribs = {}
     way_nodes = []
     tags = [] # Handle secondary tags the same way for both node and way elements
-   
 
     if element.tag=='node':
         for field in node_attr_fields:
@@ -129,7 +154,6 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
                  
         if element.find('tag') is None:
             pass
-#           print 'No Tags'
            
         elif element.find('tag') is not None:
             tag_attrib={}
@@ -141,9 +165,7 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
                     tag_attrib[node_tag_fields[0]]=element.get('id')
                     tag_attrib[node_tag_fields[1]]=tag.get('k')[(tag.get('k').find(':')+1):]
                     if tag.attrib['k']== "addr:street":
-                        tag_attrib[node_tag_fields[2]]=audit_street_name_tag(tag)
-                    elif tag.attrib['k']== "addr:postcode":
-                        tag_attrib[node_tag_fields[2]]=audit_postcode_tag(tag)       
+                        tag_attrib[node_tag_fields[2]]=audit_street_name_tag(tag)      
                     else:
                         tag_attrib[node_tag_fields[2]]=tag.get('v')
                     tag_attrib[node_tag_fields[3]]=tag.get('k').split(':')[0]
@@ -153,16 +175,14 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
                     tag_attrib[node_tag_fields[0]]=element.get('id')
                     tag_attrib[node_tag_fields[1]]=tag.get('k')
                     if tag.attrib['k']== "addr:street":
-                        tag_attrib[node_tag_fields[2]]=audit_street_name_tag(tag)
-                    elif tag.attrib['k']== "addr:postcode":
-                        tag_attrib[node_tag_fields[2]]=audit_postcode_tag(tag)    
+                        tag_attrib[node_tag_fields[2]]=audit_street_name_tag(tag)  
                     else:    
                         tag_attrib[node_tag_fields[2]]=tag.get('v')
                     tag_attrib[node_tag_fields[3]]=default_tag_type
                     tags.append(tag_attrib.copy())
             
-#        pprint.pprint( {'node':node_attribs,'node_tags':tags})  
         
+                
     elif element.tag=='way':
         for field in way_attr_fields:
             way_attribs[field]=element.get(field)
@@ -174,14 +194,10 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
             way_node_attrib[way_node_fields[1]]=nd.get('ref')
             way_node_attrib[way_node_fields[2]]=element.findall('nd').index(nd)
             way_nodes.append(way_node_attrib.copy())
-#       pprint.pprint({'way':way_attribs,'way_nodes':way_nodes})
-        
-        
         
         
         if element.find('tag') is None:
             pass
-#           print 'No Tags'
            
         elif element.find('tag') is not None:
             way_tag_attrib={}
@@ -193,9 +209,7 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
                     way_tag_attrib[way_tag_fields[0]]=element.get('id')
                     way_tag_attrib[way_tag_fields[1]]=tag.get('k')[(tag.get('k').find(':')+1):]
                     if tag.attrib['k']== "addr:street":
-                        way_tag_attrib[way_tag_fields[2]]=audit_street_name_tag(tag)
-                    elif tag.attrib['k']== "addr:postcode":
-                        way_tag_attrib[way_tag_fields[2]]=audit_postcode_tag(tag)    
+                        way_tag_attrib[way_tag_fields[2]]=audit_street_name_tag(tag)   
                     else:
                         way_tag_attrib[way_tag_fields[2]]=tag.get('v')
                     way_tag_attrib[way_tag_fields[3]]=tag.get('k').split(':')[0]
@@ -206,32 +220,17 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
                     way_tag_attrib[way_tag_fields[1]]=tag.get('k')
                     if tag.attrib['k']== "addr:street":
                         way_tag_attrib[way_tag_fields[2]]=audit_street_name_tag(tag) 
-                    elif tag.attrib['k']== "addr:postcode":
-                        way_tag_attrib[way_tag_fields[2]]=audit_postcode_tag(tag)    
                     else:   
                         way_tag_attrib[way_tag_fields[2]]=tag.get('v')
                     way_tag_attrib[way_tag_fields[3]]=default_tag_type
                     tags.append(way_tag_attrib.copy())
-#        pprint.pprint({'way':way_attribs,'way_tags':tags})
-#        pprint.pprint({'way':way_attribs,'way_nodes':way_nodes,'way_tags':tags})
-        
-    
 
     
     if element.tag == 'node':
         return {'node': node_attribs, 'node_tags': tags}
     elif element.tag == 'way':
         return {'way': way_attribs, 'way_nodes': way_nodes, 'way_tags': tags}
-        
-        
-def get_element(filename, tags=('node', 'way', 'relation')):
-    context = iter(ET.iterparse(filename, events=('start', 'end')))
-    _, root = next(context)
-    for event, elem in context:
-        if event == 'end' and elem.tag in tags:
-            yield elem
-            root.clear()  
-            
+
 def validate_element(element, validator, schema=SCHEMA):
     """Raise ValidationError if element does not match schema"""
     if validator.validate(element, schema) is not True:
@@ -252,7 +251,7 @@ class UnicodeDictWriter(csv.DictWriter, object):
     def writerows(self, rows):
         for row in rows:
             self.writerow(row)
-
+            
 def process_map(file_in, validate):
     """Iteratively process each XML element and write to csv(s)"""
 
@@ -290,4 +289,4 @@ def process_map(file_in, validate):
                     way_nodes_writer.writerows(el['way_nodes'])
                     way_tags_writer.writerows(el['way_tags'])
                     
-process_map(OSM_FILE, validate=True)                    
+process_map(dataset, validate=True)
